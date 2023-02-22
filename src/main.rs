1ee625f31e7019929e6b9d1df88b4c51a353ddc3
lilith archmachine <lilith.alanis.255@gmail.com>
2023-02-21 18:24:06 -0600

    [[good commit practice goes here]]

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ mod cellular;
 mod terminal;
 
 use terminal::MakeBufferCell;
-use cellular::*;
+use cellular::foggy; 
 
 use std::{time::Duration, io};
 use tui::{
     backend::CrosstermBackend,
-    widgets::Widget,
     Terminal
 };
 
@@ -17,33 +16,9 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
-impl<T, const WIDTH: usize, const HEIGHT: usize> Widget for &Board<T,WIDTH, HEIGHT>
-where
-    T: Copy + Default + cellular::Cell + terminal::MakeBufferCell,
-{
-    fn render(self, r: tui::layout::Rect, b: &mut tui::buffer::Buffer){
-
-        let min_i = r.width.min(self.width() as u16);
-        let min_j = r.height.min(self.height() as u16);
-
-
-        for i in 0..min_i{
-            for j in 0..min_j{
-                let x = i + r.x;
-                let y = j + r.y;
-
-                let new_cell = self.get(x as isize, y as isize).make_buffer_cell();
-                *b.get_mut(2 * x + 0, y) = new_cell.clone();
-                *b.get_mut(2 * x + 1, y) = new_cell.clone();
-
-            }
-        }
-
-    }
-}
 
 fn main() -> Result<(), io::Error> {
-    let mut board : Board<cellular::LifeCell, 32, 32> = Board::random();
+    let mut board : foggy::Board<foggy::LifeCell, 32, 32> = foggy::Board::random();
 
     // setup terminal
     enable_raw_mode()?;
